de123bc9c354498a24432929f4b4df10c3b170c1
[jerome13011] <[fanniojerome@free.fr]>
2015-12-15 15:16:25 +0100

    ajout pixel js perso, ajout structure html

---
+++ js/script.js
@@ -0,0 +1,187 @@
+window.onload = function() {
+
+  var canvas = document.getElementById('myCanvas');
+
+  paper.setup(canvas);
+
+  var Path = paper.Path;
+
+  var ctx = canvas.getContext("2d");
+
+  var pix1 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#D2691E',
+    center: [170, 110],
+  });
+  var pix2 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#D2691E',
+    center: [170, 80],
+  });
+  var pix3 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#D2691E',
+    center: [200, 80],
+  });
+  var pix4 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#D2691E',
+    center: [200, 110],
+  });
+  var pix5 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [140, 140],
+  });
+  var pix6 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [230, 140],
+  });
+  var pix7 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [200, 140],
+  });
+  var pix8 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [170, 140],
+  });
+  var pix9 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [140, 170],
+  });
+  var pix10 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [230, 170],
+  });
+  var pix11 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [170, 170],
+  });
+  var pix12 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [200, 170],
+  });
+  var pix13 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#D2691E',
+    center: [140, 200],
+  });
+  var pix14 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#D2691E',
+    center: [230, 200],
+  });
+  var pix15 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [200, 200],
+  });
+  var pix16 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#EEE8AA',
+    center: [170, 200],
+  });
+  var pix17 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#696969',
+    center: [200, 230],
+  });
+  var pix18 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#696969',
+    center: [170, 230],
+  });
+  var pix19 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#696969',
+    center: [170, 260],
+  });
+  var pix20 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: '#696969',
+    center: [200, 260],
+  });
+  var pix21 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: 'black',
+    center: [200, 290],
+  });
+  var pix22 = new Path.Rectangle({
+    size: [30, 30],
+    point: (50),
+    strokeWidth: 0.2,
+    strokeColor: 'black',
+    fillColor: 'black',
+    center: [170, 290],
+  });
+}
