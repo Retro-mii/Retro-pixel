5beb0ba5a5a492dae9f6e18940131ccc0b7679ce
[kevin.lachaise@gmail.com] <kevin.lachaise@gmail.com>
2015-12-15 23:13:32 +0100

    test animation=false

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ window.onload = function() {
   var Path = paper.Path;
 
   var ctx = canvas.getContext("2d");
-
+  
   var pix1 = new Path.Rectangle({
     size: [30, 30],
     point: (50),
@@ -184,4 +184,49 @@ window.onload = function() {
     fillColor: 'black',
     center: [395, 525],
   });
+    
+    
+    var size = new Size(30, 20);
+    
+  var posDepart = [pix1,pix2,pix3,pix4,pix5,pix6,pix7,pix8,pix9,pix10,pix11,pix12,pix13,pix14,pix15,pix16,pix17,pix18,pix19,pix20,pix21,pix22];
+  var posOrigin = [pix1,pix2,pix3,pix4,pix5,pix6,pix7,pix8,pix9,pix10,pix11,pix12,pix13,pix14,pix15,pix16,pix17,pix18,pix19,pix20,pix21,pix22];
+  var posDestination = [pix1,pix2,pix3,pix4,pix5,pix6,pix7,pix8,pix9,pix10,pix11,pix12,pix13,pix14,pix15,pix16,pix17,pix18,pix19,pix20,pix21,pix22];   
+  var clic = 0;
+  var hitOptions = {
+			segments: true,
+			stroke: true,
+			fill: true,
+			tolerance: 0
+		};
+  function onMouseUp(event) {
+			clic=!clic;
+		}
+  function onMouseMove(event) {
+			if(clic==0){
+				var hitResult = project.hitTest(event.point, hitOptions);
+				if(hitResult && hitResult.type=="fill" && posDepart.indexOf(hitResult.item)<0){
+					posDepart.push(hitResult.item);
+					posOrigin.push(hitResult.item.position);
+					posDestination.push( (Point.random() * view.size) - {x:(view.bounds.width/2)-((size.width * colSize.width)/2)-size.width,y:(view.bounds.height/2)-((size.height * colSize.width)/2)-size.height} );
+				}
+			}
+		}
+    function onFrame(event) {
+			if(clic==0){
+				for(i=0;i<posDepart.length;i++){
+					var vector = posDestination[i] - posDepart[i].position;
+					posDepart[i].position += vector / 20;
+					if (vector.length < 6) {
+						posDestination[i] = Point.random() * view.size;
+					}
+				}
+			}
+			else{
+				for(i=0;i<posDepart.length;i++){
+					var vector = posOrigin[i] - posDepart[i].position;
+					posDepart[i].position += vector / 5;
+				}
+			}
+		}
+        project.activeLayer.position = view.center;
 }
