ae852f45ec6d5f655f20bc81fce94c7934152cb8
[kevin.lachaise@gmail.com] <kevin.lachaise@gmail.com>
2015-12-15 21:55:44 +0100

    Positionement pixel, centrage de l'image, réduction de la surface pour suport télé

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#D2691E',
-    center: [170, 110],
+    center: [365, 315],
   });
   var pix2 = new Path.Rectangle({
     size: [30, 30],
@@ -22,7 +22,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#D2691E',
-    center: [170, 80],
+    center: [395, 315],
   });
   var pix3 = new Path.Rectangle({
     size: [30, 30],
@@ -30,7 +30,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#D2691E',
-    center: [200, 80],
+    center: [365, 345],
   });
   var pix4 = new Path.Rectangle({
     size: [30, 30],
@@ -38,7 +38,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#D2691E',
-    center: [200, 110],
+    center: [395, 345],
   });
   var pix5 = new Path.Rectangle({
     size: [30, 30],
@@ -46,7 +46,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [140, 140],
+    center: [335, 375],
   });
   var pix6 = new Path.Rectangle({
     size: [30, 30],
@@ -54,7 +54,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [230, 140],
+    center: [365, 375],
   });
   var pix7 = new Path.Rectangle({
     size: [30, 30],
@@ -62,7 +62,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [200, 140],
+    center: [395, 375],
   });
   var pix8 = new Path.Rectangle({
     size: [30, 30],
@@ -70,7 +70,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [170, 140],
+    center: [425, 375],
   });
   var pix9 = new Path.Rectangle({
     size: [30, 30],
@@ -78,7 +78,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [140, 170],
+    center: [335, 405],
   });
   var pix10 = new Path.Rectangle({
     size: [30, 30],
@@ -86,7 +86,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [230, 170],
+    center: [365, 405],
   });
   var pix11 = new Path.Rectangle({
     size: [30, 30],
@@ -94,7 +94,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [170, 170],
+    center: [395, 405],
   });
   var pix12 = new Path.Rectangle({
     size: [30, 30],
@@ -102,7 +102,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [200, 170],
+    center: [425, 405],
   });
   var pix13 = new Path.Rectangle({
     size: [30, 30],
@@ -110,7 +110,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#D2691E',
-    center: [140, 200],
+    center: [335, 435],
   });
   var pix14 = new Path.Rectangle({
     size: [30, 30],
@@ -118,7 +118,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#D2691E',
-    center: [230, 200],
+    center: [425, 435],
   });
   var pix15 = new Path.Rectangle({
     size: [30, 30],
@@ -126,7 +126,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [200, 200],
+    center: [395, 435],
   });
   var pix16 = new Path.Rectangle({
     size: [30, 30],
@@ -134,7 +134,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#EEE8AA',
-    center: [170, 200],
+    center: [365, 435],
   });
   var pix17 = new Path.Rectangle({
     size: [30, 30],
@@ -142,7 +142,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#696969',
-    center: [200, 230],
+    center: [365, 465],
   });
   var pix18 = new Path.Rectangle({
     size: [30, 30],
@@ -150,7 +150,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#696969',
-    center: [170, 230],
+    center: [395, 465],
   });
   var pix19 = new Path.Rectangle({
     size: [30, 30],
@@ -158,7 +158,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#696969',
-    center: [170, 260],
+    center: [365, 495],
   });
   var pix20 = new Path.Rectangle({
     size: [30, 30],
@@ -166,7 +166,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: '#696969',
-    center: [200, 260],
+    center: [395, 495],
   });
   var pix21 = new Path.Rectangle({
     size: [30, 30],
@@ -174,7 +174,7 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: 'black',
-    center: [200, 290],
+    center: [365, 525],
   });
   var pix22 = new Path.Rectangle({
     size: [30, 30],
@@ -182,6 +182,6 @@ window.onload = function() {
     strokeWidth: 0.2,
     strokeColor: 'black',
     fillColor: 'black',
-    center: [170, 290],
+    center: [395, 525],
   });
 }
