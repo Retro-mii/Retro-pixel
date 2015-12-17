6689522267a23eef73e01396e6ae136b298c7b23
[jerome13011] <[fanniojerome@free.fr]>
2015-12-17 12:26:27 +0100

    ajout animation step3et destination

--- js/script.js
+++ js/script.js
@@ -189,14 +189,35 @@ window.onload = function() {
 
   var ok = new Path.Rectangle({
     size: [30, 30],
-    point:[395,525],
+    point: [395, 525],
     fontSize: 30,
-    fillColor: 'black'
+    fillColor: 'white'
   });
-    ok.position.x= 395;
 
   var clic = 0;
   var destination = Point.random().multiply(view.size);
+  var destination1 = Point.random().multiply(view.size);
+  var destination2 = Point.random().multiply(view.size);
+  var destination3 = Point.random().multiply(view.size);
+  var destination4 = Point.random().multiply(view.size);
+  var destination5 = Point.random().multiply(view.size);
+  var destination6 = Point.random().multiply(view.size);
+  var destination7 = Point.random().multiply(view.size);
+  var destination8 = Point.random().multiply(view.size);
+  var destination9 = Point.random().multiply(view.size);
+  var destination10 = Point.random().multiply(view.size);
+  var destination11 = Point.random().multiply(view.size);
+  var destination12 = Point.random().multiply(view.size);
+  var destination13 = Point.random().multiply(view.size);
+  var destination14 = Point.random().multiply(view.size);
+  var destination15 = Point.random().multiply(view.size);
+  var destination16 = Point.random().multiply(view.size);
+  var destination17 = Point.random().multiply(view.size);
+  var destination18 = Point.random().multiply(view.size);
+  var destination19 = Point.random().multiply(view.size);
+  var destination20 = Point.random().multiply(view.size);
+  var destination21 = Point.random().multiply(view.size);
+
   view.onMouseUp = function(event) {
     clic = !clic;
   }
@@ -207,12 +228,24 @@ window.onload = function() {
       ok.position = ok.position.add(vector.divide(10));
       ok.content = Math.round(vector.length);
     } else {
-        ok.position.x = 395;
-        ok.position.y = 525;
+      ok.position.x = 395;
+      ok.position.y = 525;
     }
     if (vector.length < 5) {
       destination = Point.random().multiply(view.size);
     }
 
+  // Each frame, rotate the path by 3 degrees:
+  var vector1 = destination1.subtract(pix1.position);
+  if (clic == 0) {
+    pix1.position = pix1.position.add(vector1.divide(10));
+    pix1.content = Math.round(vector.length);
+  } else {
+    pix1.position.x = 365;
+    pix1.position.y = 315;
   }
+  if (vector1.length < 5) {
+    destination1 = Point.random().multiply(view.size);
+  }
+}
 }
